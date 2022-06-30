53fa39616ca62ff7966188a357ba4b4afb6c3cea
michaelvuolo <michaelvuolo1@gmail.com>
2022-06-30 17:05:50 -0400

    off to heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 //const mongoose = require('mongoose');
 //const morgan = require('morgan');
 //const cors = require('cors');
-//const path = require('path');
+const path = require('path');
 
 const app = express();
 
