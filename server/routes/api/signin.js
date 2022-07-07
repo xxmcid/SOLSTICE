1024e4b2c74d36675329f8337a640dec7a73d6ac
michaelvuolo <michaelvuolo1@gmail.com>
2022-07-07 14:10:53 -0400

    fixed import

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const md5 = require('md5');
 const User = require("../../models/User");
+const attributes = require('../../resources/attribute-validation');
 
 // Initialize express
 const app = express();
