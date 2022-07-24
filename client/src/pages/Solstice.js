734ed9e0f84e2fcf22bf58111b1278f331a7a99f
Isaac Liljeros <isaac.liljeros@knights.ucf.edu>
2022-07-23 21:52:20 -0400

    Top bar

--- client/src/pages/Solstice.js
+++ client/src/pages/Solstice.js
@@ -37,7 +37,6 @@ class Solstice extends Component
 
             // cache all solar systems
             solarSystems: [],
-            solarSystemButtonObjs: [],
 
             // Solstice States
             solarSystemId: '',
@@ -77,8 +76,6 @@ class Solstice extends Component
                 planets: planetsArray,
                 solarSystemId: solarSystems[0]?._id
             });
-
-            this.generateSolarSystemButtonObjs();
         })
         .catch(err => {
             console.log(err);
@@ -98,6 +95,24 @@ class Solstice extends Component
                     });
                 }
             }
+        });
+    }
+
+    refreshSolarSystems() {
+        const url = `${window.location.protocol}//${window.location.host}/api/fetch-solar-systems/${this.state.clientSession}`
+        
+        // Fetch the user's solar systems.
+        axios.get(url).then(response => {
+            let solarSystems = response.data.solarSystems;
+
+            // Set our planets JSON to our state
+            // P5 should see this change in sketch.js and update accordingly.
+            this.setState({
+                solarSystems: solarSystems,
+            });
+        }).catch(err => {
+            console.log(err);
+            console.log('COULD NOT FIND ANY SOLAR SYSTEMS FOR THE USER!!!');
         });
     }
 
@@ -208,6 +223,8 @@ class Solstice extends Component
                     <AppHeader 
                         solarSystems={this.state.solarSystems} 
                         switchSolarSystem={id => this.updateSelectedSolarSystem(id)}
+                        clientSession={this.state.clientSession}
+                        refreshSolarSystems={() => this.refreshSolarSystems()}
                     />
 
                     <SidePanel 
